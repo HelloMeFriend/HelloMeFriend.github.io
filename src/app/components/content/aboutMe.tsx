1c1fa8c076c56b28ac82f80e7ca2e868139951d1
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2026-01-05 13:34:32 -0500

    Update personal description in AboutContent component

--- src/app/components/content/aboutMe.tsx
+++ src/app/components/content/aboutMe.tsx
@@ -3,11 +3,11 @@ import Text from "../text";
 export default function AboutContent() {
     return (
         <Text size="small" style={{ textAlign: 'justify' }}>
-            Hello! I&apos;m Omar ALSHANYOUR, a passionate software developer with a knack for creating. I&apos;m a recent alumnus of Concordia University with a bachelors degree in Software Engineering.
+            Hello! I&apos;m Omar ALSHANYOUR, a passionate software developer and creative. I&apos;m a recent alumnus of Concordia University with a bachelors degree in Software Engineering.
             Lately, I&apos;ve been really passionate about cloud computing and web development. I also like to spend a bit of time delving into the world of game dev. As for AI, I think I&apos;ll leave
             it to the experts and focus on learning how to use it effectively and ethically in my projects. <br /><br />
 
-            Outside of coding, I greatly enjoy immersing myself in different sorts of media and arts, as I feel it&apos;s important to nurture one&apos;s understanding of the world. I also enjoy hiking, football (soccer), and going to the gym. My current favourite song is <i>Harvest Moon</i> by Neil Young. Current favourite movie is
+            Outside of coding, I greatly enjoy immersing myself in different sorts of media and arts, as I feel it&apos;s important to nurture one&apos;s personal development. I also enjoy hiking, football (soccer), and going to the gym. My current favourite song is <i>Harvest Moon</i> by Neil Young. Current favourite movie is
             <i> How to Make Millions Before Grandma Dies</i> from Pat Boonnitipat. Current favourite book is <i>Native Son</i> by Richard Wright. <br /><br />
 
             Feel free to reach out if you&apos;d like to connect or collaborate on something exciting!
